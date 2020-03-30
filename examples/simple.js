d96d08f86c0322f74e0d2e7e964cded420eb10d4
bansky <13430293505@163.com>
2020-03-30 17:52:06 +0800

    添加set的时候，对null的处理

--- examples/simple.js
+++ examples/simple.js
@@ -1,40 +1,41 @@
 const mdb = require("../index")
 
-async function main()
-{
-    const client = await mdb.connect("mysql://root:123456@192.168.1.4",{log:console.log})
+async function main() {
+    const client = await mdb.connect("mysql://root:123456@192.168.1.4", { log: console.log })
 
     const db = client.db("project")
 
     const collection = db.collection("user")
 
-    collection.createIndex({name:1},{unique:1})
-    collection.createIndex({union:1,member:1},{unique:1})
+    collection.createIndex({ name: 1 }, { unique: 1 })
+    collection.createIndex({ union: 1, member: 1 }, { unique: 1 })
 
-    collection.insertOne({ _id: 1, name: "张三",union:1,member:3,lvl:10 })
-    collection.updateOne({ _id: 2 }, { $set: { name: "李四",union:2,member:3,lvl:10 } }, { upsert: 1 })
+    collection.insertOne({ _id: 1, name: "张三", union: 1, member: 3, lvl: 10, temp: "this is a test" })
+    collection.updateOne({ _id: 2 }, { $set: { name: "李四", union: 2, member: 3, lvl: 10 } }, { upsert: 1 })
 
     let cursor = await collection.find({})
 
     console.table(cursor.toArray())
 
+    collection.updateOne({ _id: 1 }, { $set: { is_new: true, temp: null } })
+
     let data = await collection.findOne({ _id: 1 })
 
     console.dir(data)
 
-    data = await collection.findOne({ name: "张三" },{projection:{name:1}})
+    data = await collection.findOne({ name: "张三" }, { projection: { name: 1 } })
 
     console.dir(data)
 
-    data = await collection.findOne({ union: 1,member:3 },{projection:{name:1}})
+    data = await collection.findOne({ union: 1, member: 3 }, { projection: { name: 1 } })
 
     console.dir(data)
 
-    data = await collection.findOne({ lvl:10 },{projection:{name:1}})
+    data = await collection.findOne({ lvl: 10 }, { projection: { name: 1 } })
 
     console.dir(data)
 
-    await collection.deleteOne({_id:1})
+    await collection.deleteOne({ _id: 1 })
 
     cursor = await collection.find({})
 
@@ -43,37 +44,36 @@ async function main()
     await collection.deleteMany({})
 }
 
-async function test_auto()
-{
-    const client = await mdb.connect("mysql://root:123456@192.168.1.4",{log:console.log})
+async function test_auto() {
+    const client = await mdb.connect("mysql://root:123456@192.168.1.4", { log: console.log })
 
     const db = client.db("project")
 
     const collection = db.collection("auto")
 
-    collection.createIndex({name:1},{unique:1})
-    collection.createIndex({union:1,member:1},{unique:1})
+    collection.createIndex({ name: 1 }, { unique: 1 })
+    collection.createIndex({ union: 1, member: 1 }, { unique: 1 })
 
-    collection.insertOne({name: "张三",union:1,member:3,lvl:10 })
-    collection.insertOne({name: "李四",union:2,member:3,lvl:5 })
+    collection.insertOne({ name: "张三", union: 1, member: 3, lvl: 10, is_new: true })
+    collection.insertOne({ name: "李四", union: 2, member: 3, lvl: 5 })
 
     let cursor = await collection.find({})
 
     console.table(cursor.toArray())
 
-    let data = await collection.findOne({ name: "张三" },{projection:{name:1}})
+    let data = await collection.findOne({ name: "张三" }, { projection: { name: 1 } })
 
     console.dir(data)
 
-    data = await collection.findOne({ union: 1,member:3 },{projection:{name:1}})
+    data = await collection.findOne({ union: 1, member: 3 }, { projection: { name: 1 } })
 
     console.dir(data)
 
-    data = await collection.findOne({ lvl:10 },{projection:{name:1}})
+    data = await collection.findOne({ lvl: 10 }, { projection: { name: 1 } })
 
     console.dir(data)
 
-    await collection.deleteOne({_id:1})
+    await collection.deleteOne({ _id: 1 })
 
     cursor = await collection.find({})
 
@@ -84,4 +84,4 @@ async function test_auto()
 
 main()
 
-test_auto()
+// test_auto()
--- src/utils/mysql.js
+++ src/utils/mysql.js
@@ -1,30 +1,28 @@
 module.exports.select_page = 3000
 
-const default_cols = {_id:1,_content:1,_updated:1,_inserted:1}
+const default_cols = { _id: 1, _content: 1, _updated: 1, _inserted: 1 }
 
 module.exports.DATATYPE = {
-    TEXT:"text",
-    VARCHAR:"varchar(32)",
-    INT:"int(11)",
-    BIGINT:"bigint(20)",
-    JSON:"json",
-    TIMESTAMP:"timestamp",
+    TEXT: "text",
+    VARCHAR: "varchar(32)",
+    INT: "int(11)",
+    BIGINT: "bigint(20)",
+    JSON: "json",
+    TIMESTAMP: "timestamp",
 }
 
 module.exports.DEFAULT_VALUE = {
-    CURRENT_TIME:"CURRENT_TIMESTAMP",
+    CURRENT_TIME: "CURRENT_TIMESTAMP",
 }
 
 module.exports.EXTRA = {
-    VIRTUAL_JSON:"VIRTUAL GENERATED",
-    UPDATE_TIMESTAMP:"ON UPDATE CURRENT_TIMESTAMP",
+    VIRTUAL_JSON: "VIRTUAL GENERATED",
+    UPDATE_TIMESTAMP: "ON UPDATE CURRENT_TIMESTAMP",
 }
 
 
-function is_json_key(key)
-{
-    if(default_cols[key])
-    {
+function is_json_key(key) {
+    if (default_cols[key]) {
         return false
     }
     return true
@@ -32,28 +30,28 @@ function is_json_key(key)
 
 module.exports.is_json_key = is_json_key
 
-module.exports.json_extract = (key)=>
-{
-    if(is_json_key(key))
-    {
+module.exports.json_extract = (key) => {
+    if (is_json_key(key)) {
         return `_content->>'$.${key}'`
     }
     return `\`${key}\``
 }
 
-module.exports.json_set = (col_name,values)=>
-{
+module.exports.json_set = (col_name, values) => {
     let sqls = []
 
-    for(let key in values)
-    {
+    for (let key in values) {
         let val = values[key]
-        let tp = typeof(val)
+        let tp = typeof (val)
 
         sqls.push(`'$.${key}'`)
 
-        switch(tp)
-        {
+        if (val == null) {
+            sqls.push(`CAST('null' AS JSON)`)         //注意这里的不同
+            continue
+        }
+
+        switch (tp) {
             case "string":
                 sqls.push(`"${val}"`)
                 break
@@ -61,38 +59,34 @@ module.exports.json_set = (col_name,values)=>
                 sqls.push(`CAST('${JSON.stringify(val)}' AS JSON)`)         //注意这里的不同
                 break
             default:
-                sqls.push(val.toString() )
+                sqls.push(val.toString())
                 break
-        }  
+        }
     }
 
     return `${col_name}=JSON_SET(${col_name},${sqls.join(",")})`
 }
 
 
-function string_val(val)
-{
-    let tp = typeof(val)
-    switch(tp)
-    {
+function string_val(val) {
+    let tp = typeof (val)
+    switch (tp) {
         case "string":
             return `"${val}"`
         case "object":
             return `'${JSON.stringify(val)}'`
         default:
-            return val.toString()     
+            return val.toString()
             break
-    }   
+    }
 }
 
 module.exports.string_val = string_val
 
-module.exports.set_columns = (values)=>
-{
+module.exports.set_columns = (values) => {
     let sqls = []
 
-    for(let key in values)
-    {
+    for (let key in values) {
         let val = values[key]
 
         sqls.push(`\`${key}\`=${string_val(val)}`)
@@ -101,18 +95,16 @@ module.exports.set_columns = (values)=>
     return sqls.join(",")
 }
 
-module.exports.make_projection = (data)=>
-{
+module.exports.make_projection = (data) => {
     let content = data._content
 
     delete data._content
 
-    if(content == null)
-    {
+    if (content == null) {
         return data
     }
 
-    Object.assign(data,content)
+    Object.assign(data, content)
 
     return data
 }
